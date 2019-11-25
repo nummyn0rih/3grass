9ac230e4a68a3b518de38b9165cbd396c86b0bd1
Ivan Malshchukov <nummyn0rih@gmail.com>
2019-11-25 14:11:41 +0300

    fix linter problems

--- gulpfile.js
+++ gulpfile.js
@@ -1,19 +1,18 @@
-var gulp = require('gulp');
-var sass = require('gulp-sass');
-var babel = require("gulp-babel");
+const gulp = require('gulp');
+const sass = require('gulp-sass');
+const babel = require('gulp-babel');
 
-gulp.task('sass', function () {
+gulp.task('sass', () => {
   return gulp.src('./src/sass/**/*.scss')
     .pipe(sass().on('error', sass.logError))
     .pipe(gulp.dest('./public/css'))
 });
 
-gulp.task('watch', function () {
-    gulp.watch('.src/sass/**/*.scss', gulp.series('sass'));
+gulp.task('watch', () => {
+  gulp.watch('.src/sass/**/*.scss', gulp.series('sass'));
 });
 
-gulp.task("default", function () {
-  return gulp.src("src/app.js")
-    .pipe(babel())
-    .pipe(gulp.dest("dist"));
-});
+gulp.task('default', () => gulp.src('src/app.js')
+  .pipe(babel())
+  .pipe(gulp.dest('dist'))
+);
